99346e41ef14ea33fe99388afb0ab85b4e09c45b
samuel <20240528@ricaldone.edu.sv>
2025-08-19 19:25:10 -0600

    finalizado

---
+++ js/services/imageService.js
@@ -0,0 +1,27 @@
+const IMAGE_API = 'http://localhost:8080/api/image';
+
+//Sube una imagen y devuelve el JSON del backend: { message, url}
+export async function UploadImage(file) {
+    const formData = new FormData();
+    formData.append('image', file); //Debe coincidir con el @RequestParam("image")
+
+    const res = await fetch(`${IMAGE_API}/upload`, {
+        method: 'POST',
+        body: FormData,
+    });
+
+    return res.json();
+}
+
+export async function uploadImageToFolder(file, folder) {
+    const formData = new FormData();
+    formData.append('image', file);
+    formData.append('folder', folder);
+
+    const res = await fetch(`${IMAGE_API}/upload-to-folder`, {
+        method: 'POST',
+        body: formData,
+    });
+    
+    return res.json();
+}
